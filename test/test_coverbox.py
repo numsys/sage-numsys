8af57f0928f7811416a59291dcffc8f962ea43bc
Aron Borbenyi <aron.borbenyi@webvalto.hu>
2021-03-15 17:39:05 +0100

    Isolate coverbox tests from unit_test.py

---
+++ test/test_coverbox.py
@@ -0,0 +1,48 @@
+from parameterized import parameterized
+from sage.all import *
+from gns import *
+
+import unittest
+
+
+class CoverBoxTestCase(unittest.TestCase):
+
+    def test_cover_box_CB1(self):
+        m = Matrix(ZZ, [[2, -1], [1, 2]])
+        digits = Digits([[0, 0], [1, 0], [0, 1], [0, -1], [-6, 5]])
+        numsys = SemiRadixSystem(m, digits)
+
+        self.assertEqual(numsys.compute_cover_box(), ([-2, -6], [2, 1]))
+
+    def test_cover_box_CB2(self):
+        m = Matrix(ZZ, [[1, -2], [1, 1]])
+        digits = Digits([[0, 0], [1, 0], [-1, 0]])
+        numsys = SemiRadixSystem(m, digits)
+
+        self.assertEqual(numsys.compute_cover_box(), ([-1, -1], [1, 1]))
+
+    def test_cover_box_CB3_symmetric_digits(self):
+        m = Matrix(ZZ, [[1, -2], [1, 1]])
+        digits = SymmetricDigits()
+        numsys = SemiRadixSystem(m, digits)
+
+        self.assertEqual(numsys.compute_cover_box(), ([-1, -1], [1, 1]))
+
+    def test_cover_box_CB4(self):
+        m = Matrix(ZZ, [[-1, -1], [1, -1]])
+        digits = Digits([[0, 0], [1, 0]])
+        numsys = SemiRadixSystem(m, digits)
+
+        self.assertEqual(numsys.compute_cover_box(), ([-1, -1], [1, 1]))
+
+    def test_cover_box_CB5(self):
+        m = Matrix(ZZ, [[3]])
+        digits = Digits([[-2], [0], [2]])
+        numsys = SemiRadixSystem(m, digits)
+
+        self.assertEqual(numsys.compute_cover_box(), ([-1], [1]))
+
+
+if __name__ == '__main__':
+    unittest.TextTestRunner(sys.stderr, True, 1, False, False, None).run(
+        unittest.TestLoader().loadTestsFromTestCase(CoverBoxTestCase))
