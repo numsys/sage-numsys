56b62413093002c8adc7d6abc82d04d624c7300e
Hudi <hudi1989@gmail.com>
2023-04-19 12:49:56 +0200

    Add det calculation to basic worker

--- distributed/workers/basic.py
+++ distributed/workers/basic.py
@@ -7,13 +7,17 @@ from distributed.server_connection import call_server, ServerJsonEncoder
 def calculate_basics(data, rs):
     volume = rs.get_cover_box_volume()
     dim = rs.get_dimension()
+    determinant = rs.determinant
+    abs_determinant = rs.abs_determinant
     call_server(BASE_URL + "add-properties",
                 {},
                 {
                     'RSId': data["id"],
                     'properties': json.dumps({
                         'volume': volume,
-                        'dimension': dim
+                        'dimension': dim,
+                        'determinant': determinant,
+                        'abs_determinant': abs_determinant,
                     }, cls=ServerJsonEncoder),
                     'token': 'asdasd',
                 }
